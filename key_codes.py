0f34fad9b1e1e99044cde9b95e18f6c4458a6157
zoey <mianmian0202@gmail.com>
2025-07-31 13:41:24 +0800

    Create

---
+++ key_codes.py
@@ -0,0 +1,13 @@
+# Windows API 常數
+WM_KEYDOWN = 0x0100
+WM_KEYUP = 0x0101
+
+# 虛擬鍵碼
+VK_CODES = {
+    'space': 0x20,
+    'left': 0x25,
+    'right': 0x27,
+    'c': 0x43,
+    'z': 0x5A,
+    '1': 0x31,
+}
